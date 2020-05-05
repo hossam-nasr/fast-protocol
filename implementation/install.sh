6ab249fed6f1d2dc8410a5fea50e30af396f600d
Hossam <hossam.nasr99@gmail.com>
2020-05-05 05:59:28 -0500

    add install and run scripts

---
+++ implementation/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip3 install PyCryptodome
+pip install PyCryptodome
+pip3 install termcolor
+pip install termcolor
+pip3 install colorama
+pip install colorama
---
+++ implementation/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gnome-terminal -- ./run_network.sh
+gnome-terminal -- ./run_server.sh
+./run_client.sh
